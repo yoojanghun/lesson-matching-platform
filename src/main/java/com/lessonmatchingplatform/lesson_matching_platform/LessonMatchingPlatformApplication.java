410f6d7b0c7f764d856eb8bc262ac12f3029d7c2
yoojanghun <yoojanghun@naver.com>
2026-01-29 11:38:24 +0900

    Gradle 환경설정

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/LessonMatchingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.lessonmatchingplatform.lesson_matching_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonMatchingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonMatchingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lessonmatchingplatform/lesson_matching_platform/LessonMatchingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessonmatchingplatform.lesson_matching_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonMatchingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

372c7953541a960ddb1bb433de770fc7840f1964
yoojanghun <yoojanghun@naver.com>
2026-02-01 00:37:34 +0900

    UserAccount Entity 생성
    index는 WHERE절에 많이 사용되거나 JOIN이 많이 일어나는 column과 같이 조회가 많이 일어나는 column에 사용
    INSERT, DELETE UPDATE가 일어나는 column엔 성능 저하로 사용 안함.
    
    enum 타입은 @Enumerated(EnumType.String)을 붙임. default는 db에 enum의 index가 저장되기 때문.

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/domain/UserAccount.java
@@ -0,0 +1,70 @@
+package com.lessonmatchingplatform.lesson_matching_platform.domain;
+
+import com.lessonmatchingplatform.lesson_matching_platform.domain.type.RoleType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@ToString(callSuper = true)
+@Getter
+@Table(indexes = {
+        @Index(columnList = "userId"),
+        @Index(columnList = "phoneNumber"),
+        @Index(columnList = "email")
+})
+@Entity
+public class UserAccount extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String userId;
+
+    @Column(length = 255, nullable = false)
+    private String userPassword;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private RoleType role;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(length = 20, nullable = false)
+    private String phoneNumber;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    protected UserAccount() {}
+
+    private UserAccount(String userId, String userPassword, RoleType role, LocalDate birthDate, String phoneNumber, String email) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.role = role;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public static UserAccount of(String userId, String userPassword, RoleType role, LocalDate birthDate, String phoneNumber, String email) {
+        return new UserAccount(userId, userPassword, role, birthDate, phoneNumber, email);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAccount that)) return false;
+        return this.id != null && Objects.equals(this.id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

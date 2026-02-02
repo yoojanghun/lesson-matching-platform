f251a2b8a40b673907a966336ba8b43141a55624
yoojanghun <yoojanghun@naver.com>
2026-02-02 15:33:35 +0900

    #16 - TutorsService layer 설계
    우선 DTO는 request, response 두 가지로만 나눔.
    나중에 DTO에서 공통되는 내용이 많아질 때 common DTO 사용 고려.

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/dto/request/TutorsRequest.java
@@ -0,0 +1,4 @@
+package com.lessonmatchingplatform.lesson_matching_platform.dto.request;
+
+public class TutorsRequest {
+}
---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/dto/response/TutorsResponse.java
@@ -0,0 +1,4 @@
+package com.lessonmatchingplatform.lesson_matching_platform.dto.response;
+
+public class TutorsResponse {
+}

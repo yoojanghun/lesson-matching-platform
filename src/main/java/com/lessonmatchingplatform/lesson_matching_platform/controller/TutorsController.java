49e04f2332b747f1bf797f979be9abdaccf928a9
yoojanghun <yoojanghun@naver.com>
2026-02-02 15:31:52 +0900

    #16 - Tutors Controller 기본 틀 작성
    Tutors를 Category, Subject 별로 분리해서 보여준 후, searchType을 이용하여 별점, 매칭 빈도, 등록 순 등으로 sorting하기 위해
    CategoryType, SubjectType, SearchType을 enum으로 작성.
    enum의 내용은 db의 내용과 동일하게 수작업으로 작성해야 함. Category와 Subject를 관리하는 admin 계정이 없기 때문

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/controller/TutorsController.java
@@ -0,0 +1,24 @@
+package com.lessonmatchingplatform.lesson_matching_platform.controller;
+
+import com.lessonmatchingplatform.lesson_matching_platform.domain.service.TutorsService;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.response.TutorsResponse;
+import com.lessonmatchingplatform.lesson_matching_platform.type.CategoryType;
+import com.lessonmatchingplatform.lesson_matching_platform.type.SearchType;
+import com.lessonmatchingplatform.lesson_matching_platform.type.SubjectType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/tutors")
+@RestController
+public class TutorsController {
+
+    @GetMapping
+    public List<TutorsResponse> getTutorsList(
+            @RequestParam CategoryType category,
+            @RequestParam SubjectType subject,
+            @RequestParam SearchType searchType
+    ) {
+        return TutorsService.getTutorsList(category, subject, searchType);
+    }
+}

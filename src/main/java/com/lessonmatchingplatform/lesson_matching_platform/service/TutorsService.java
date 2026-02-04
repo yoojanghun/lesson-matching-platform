3b42fdf57dba530b3fbe5f5ef708ed9772380343
yoojanghun <yoojanghun@naver.com>
2026-02-04 13:47:09 +0900

    # 16 - Service Layer 로직 작성
    TutorsRepository에서 querydsl을 이용하여 작성한 custom 메소드인 searchTutors를 사용.
    그것으로 반환된 TutorsAccount entity들을 TutorsResponse로 변경시킴

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/service/TutorsService.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/service/TutorsService.java
@@ -1,16 +1,24 @@
 package com.lessonmatchingplatform.lesson_matching_platform.service;
 
+import com.lessonmatchingplatform.lesson_matching_platform.dto.request.TutorSearchCondition;
 import com.lessonmatchingplatform.lesson_matching_platform.dto.response.TutorsResponse;
-import com.lessonmatchingplatform.lesson_matching_platform.type.CategoryType;
-import com.lessonmatchingplatform.lesson_matching_platform.type.SearchType;
-import com.lessonmatchingplatform.lesson_matching_platform.type.SubjectType;
+import com.lessonmatchingplatform.lesson_matching_platform.repository.TutorsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Service
 public class TutorsService {
-    public static List<TutorsResponse> getTutorsList(CategoryType category, SubjectType subject, SearchType searchType) {
 
+    private final TutorsRepository tutorsRepository;
+
+    @Transactional(readOnly = true)
+    public Page<TutorsResponse> getTutorsList(TutorSearchCondition tutorSearchCondition, Pageable pageable) {
+        return tutorsRepository.searchTutors(tutorSearchCondition, pageable)
+                .map(TutorsResponse::from);
     }
 }

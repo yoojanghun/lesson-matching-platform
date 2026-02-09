9812c87c3e592a4313149184d0d5ce0389e03f2c
yoojanghun <yoojanghun@naver.com>
2026-02-09 21:23:00 +0900

    #21 - AuditingFields에 어노테이션 추가
    @CreatedDate, @CreatedDate 등 추가함

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/domain/AuditingFields.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/domain/AuditingFields.java
@@ -5,24 +5,36 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @ToString
 @Getter
-@EntityListeners(AuditingFields.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class AuditingFields {
 
-    @Column(nullable = false)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(length = 100, nullable = false)
+    @CreatedBy
+    @Column(length = 100, nullable = false, updatable = false)
     private String createdBy;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
     @Column
     private LocalDateTime modifiedAt;
 
+    @LastModifiedBy
     @Column(length = 100)
     private String modifiedBy;
 }

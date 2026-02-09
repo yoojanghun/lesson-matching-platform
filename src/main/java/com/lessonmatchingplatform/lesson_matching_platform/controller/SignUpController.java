e48a7b4625c48d7506c78d772dfad9aa297d7cff
yoojanghun <yoojanghun@naver.com>
2026-02-09 21:31:47 +0900

    #21 회원가입용 controller 설계

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/controller/SignUpController.java
@@ -0,0 +1,37 @@
+package com.lessonmatchingplatform.lesson_matching_platform.controller;
+
+import com.lessonmatchingplatform.lesson_matching_platform.domain.account.StudentAccount;
+import com.lessonmatchingplatform.lesson_matching_platform.domain.account.TutorAccount;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.request.TutorSignUpRequest;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.response.TutorResponse;
+import com.lessonmatchingplatform.lesson_matching_platform.service.SignUpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/sign-up")
+@RestController
+public class SignUpController {
+
+    private final SignUpService signUpService;
+
+    // Tutor로 처음 sign up 할 때
+    @PostMapping("/tutor")
+    public TutorResponse signUpTutor(
+            @RequestBody TutorSignUpRequest tutorSignUpRequest
+    ) {
+        return signUpService.signUpTutor(tutorSignUpRequest);
+    }
+
+    // Student로 처음 sign up 할 때
+//    @PostMapping("/student")
+//    public StudentAccount signUpStudent() {
+//
+//    }
+
+}
+
+// Student와 Tutor로 각각 회원가입 할 때, 일부 공통된 정보는 UserAccount에 저장하도록

b8a438db80fe77886a87c3dbbf28e6ae33478a4a
yoojanghun <yoojanghun@naver.com>
2026-02-09 21:17:04 +0900

    #21 - BoardPrincipal 작성
    UserDetails: 사용자의 핵심 정보를 담는 그릇
    스프링 시큐리티는 사용자가 누구인지, 비밀번호는 무엇인지, 어떤 권한을 가졌는지 확인해야 인증을 진행할 수 있는데,
    이를 위해 최소한으로 필요한 규격을 정해 놓은 것
    이를 custom 구현한 BoardPrincipal을 만들어, DB를 조회하지 않고 로그인 된 유저의 정보를 바로 얻어낼 수 있게 만듦.
    getPhoneNumber(), getEmail() 등

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/dto/security/BoardPrincipal.java
@@ -0,0 +1,50 @@
+package com.lessonmatchingplatform.lesson_matching_platform.dto.security;
+import com.lessonmatchingplatform.lesson_matching_platform.domain.account.UserAccount;
+import com.lessonmatchingplatform.lesson_matching_platform.type.GenderType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+public record BoardPrincipal(
+        String username,
+        String password,
+        Collection<? extends GrantedAuthority> authorities,
+        String name,
+        GenderType gender,
+        LocalDate birthDate,
+        String phoneNumber,
+        String email
+) implements UserDetails {
+
+    public static BoardPrincipal from(UserAccount entity) {
+
+        // 사용자의 권한 리스트 (ROLE_TUTOR, ROLE_STUDENT 등)
+        Collection<SimpleGrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority("ROLE_" + entity.getRole().name())
+        );
+
+        return new BoardPrincipal(
+                entity.getUserId(),
+                entity.getUserPassword(),
+                authorities,
+                entity.getName(),
+                entity.getGender(),
+                entity.getBirthDate(),
+                entity.getPhoneNumber(),
+                entity.getEmail()
+        );
+    }
+
+    @Override public Collection<? extends GrantedAuthority> getAuthorities() {return authorities;}
+    @Override public String getPassword() {return password;}
+    @Override public String getUsername() {return username;}
+    @Override public boolean isAccountNonExpired() {return true;}
+    @Override public boolean isAccountNonLocked() {return true;}
+    @Override public boolean isCredentialsNonExpired() {return true;}
+    @Override public boolean isEnabled() {return true;}
+
+}

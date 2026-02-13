ce03df59d9a87a6a85442597dae197e981702bcc
yoojanghun <yoojanghun@naver.com>
2026-02-13 15:36:40 +0900

    #27 - LessonMatchingService 설계
    StudentAccount 조회를 위해 BoardPrincipal에서 id를 조회할 수 있는 필드 추가.
    StudentAccount, TutorAccount의 내용은 필요 없고 pk만 필요하므로, getReferenceById로 proxy 객체만 가져옴. (쿼리를 날리지 않아 효율적)
    각 메소드들을 Transactional로 묶어 데이터베이스의 상태를 변화시키는 여러 SQL 작업을 하나의 논리적 단위로 만듦. 모든 작업이 성공(Commit)하거나 전부 실패(Rollback)하여 데이터 무결성을 보장
    
    Matching을 저장하자마자 Matching을 조회하여 타이밍이 안 맞을 것 같지만 그렇지 않음.
    애플리케이션과 데이터베이스 사이에서 엔티티를 보관하는 가상의 데이터베이스(메모리 공간)인 영속성 컨텍스트가 존재.
    영속성 컨텍스트 내부의 1차 캐시에 Matching이 저장됨. 그 후, 해당 Matching을 findById하면 DB까지 가지 않고, 영속성 컨텍스트 내부의 Matching을 돌려줌.
    따라서 @Transactional 안에선 save하자마자 entity 바로 조회 가능함.
    transaction이 끝나면 영속성 컨텍스트와 db가 동기화됨(Flush)

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/dto/security/BoardPrincipal.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/dto/security/BoardPrincipal.java
@@ -14,6 +14,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public record BoardPrincipal(
+        Long id,
         String username,
         String password,
         Collection<? extends GrantedAuthority> authorities,
@@ -35,6 +36,7 @@ public record BoardPrincipal(
                 .toList();
 
         return new BoardPrincipal(
+                entity.getId(),
                 entity.getUserId(),
                 entity.getUserPassword(),
                 authorities,
---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/service/LessonMatchingService.java
@@ -0,0 +1,39 @@
+package com.lessonmatchingplatform.lesson_matching_platform.service;
+
+import com.lessonmatchingplatform.lesson_matching_platform.domain.account.StudentAccount;
+import com.lessonmatchingplatform.lesson_matching_platform.domain.account.TutorAccount;
+import com.lessonmatchingplatform.lesson_matching_platform.domain.lesson.Matching;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.request.LessonMatchingRequest;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.response.LessonMatchingResponse;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.security.BoardPrincipal;
+import com.lessonmatchingplatform.lesson_matching_platform.repository.MatchingRepository;
+import com.lessonmatchingplatform.lesson_matching_platform.repository.StudentRepository;
+import com.lessonmatchingplatform.lesson_matching_platform.repository.TutorsRepository;
+import com.lessonmatchingplatform.lesson_matching_platform.type.MatchingStatus;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class LessonMatchingService {
+
+    private final StudentRepository studentRepository;
+    private final TutorsRepository tutorsRepository;
+    private final MatchingRepository matchingRepository;
+
+    public LessonMatchingResponse lessonMatching(BoardPrincipal boardPrincipal, Long tutorId, LessonMatchingRequest request) {
+        StudentAccount studentAccount = studentRepository.getReferenceById(boardPrincipal.id());
+        TutorAccount tutorAccount = tutorsRepository.getReferenceById(tutorId);                     // pk만 필요하므로 proxy 생성
+
+        Matching lessonMatching = Matching.of(studentAccount, tutorAccount, request.requestMsg(), MatchingStatus.PENDING);
+        Matching savedMatching = matchingRepository.save(lessonMatching);
+
+        Matching matchingDetails = matchingRepository.findByIdWithDetails(savedMatching.getMatchingId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        return LessonMatchingResponse.from(matchingDetails);
+    }
+}

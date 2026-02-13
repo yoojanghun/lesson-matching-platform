8dd601e2652b0fae64b4d643bd3ff8bf78ba7d03
yoojanghun <yoojanghun@naver.com>
2026-02-13 15:39:51 +0900

    #27 - Matching에서 EnumType을 String으로 설정
    String으로 설정 안 하면 data.sql에서 숫자로 data를 집어넣어야 함.

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/domain/lesson/Matching.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/domain/lesson/Matching.java
@@ -3,6 +3,7 @@ package com.lessonmatchingplatform.lesson_matching_platform.domain.lesson;
 import com.lessonmatchingplatform.lesson_matching_platform.domain.AuditingFields;
 import com.lessonmatchingplatform.lesson_matching_platform.domain.account.StudentAccount;
 import com.lessonmatchingplatform.lesson_matching_platform.domain.account.TutorAccount;
+import com.lessonmatchingplatform.lesson_matching_platform.type.MatchingStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.ToString;
@@ -31,22 +32,23 @@ public class Matching extends AuditingFields {
     @Column(length = 500, nullable = false)
     private String requestMsg;
 
+    @Enumerated(EnumType.STRING)
     @Column(length = 20, nullable = false)
-    private String status;
+    private MatchingStatus status;
 
     @OneToOne(mappedBy = "matching", cascade = CascadeType.ALL)
     private LessonReview lessonReview;
 
     protected Matching() {}
 
-    private Matching(StudentAccount studentAccount, TutorAccount tutorAccount, String requestMsg, String status) {
+    private Matching(StudentAccount studentAccount, TutorAccount tutorAccount, String requestMsg, MatchingStatus status) {
         this.studentAccount = studentAccount;
         this.tutorAccount = tutorAccount;
         this.requestMsg = requestMsg;
         this.status = status;
     }
 
-    public static Matching of(StudentAccount studentAccount, TutorAccount tutorAccount, String requestMsg, String status) {
+    public static Matching of(StudentAccount studentAccount, TutorAccount tutorAccount, String requestMsg, MatchingStatus status) {
         return new Matching(studentAccount, tutorAccount, requestMsg, status);
     }
 

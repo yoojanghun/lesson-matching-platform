af3b84310da26151ebd8daddc6e421ff6d16e6e8
yoojanghun <yoojanghun@naver.com>
2026-02-14 12:36:50 +0900

    #27 - Tutor가 자신의 레슨 요청들을 list로 확인할 수 있도록 하는 Controller
    @PreAuthorize를 통해 boardPrincipal에서 ROLE_TUTOR가 있는 지 확인

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/controller/LessonMatchingController.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/controller/LessonMatchingController.java
@@ -2,11 +2,15 @@ package com.lessonmatchingplatform.lesson_matching_platform.controller;
 
 import com.lessonmatchingplatform.lesson_matching_platform.dto.request.LessonMatchingRequest;
 import com.lessonmatchingplatform.lesson_matching_platform.dto.response.LessonMatchingResponse;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.response.MyMatchingResponse;
 import com.lessonmatchingplatform.lesson_matching_platform.dto.security.BoardPrincipal;
 import com.lessonmatchingplatform.lesson_matching_platform.service.LessonMatchingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/tutors")              // 자원의 계층 구조를 명확히 함(tutors)
@@ -15,6 +19,7 @@ public class LessonMatchingController {
 
     private final LessonMatchingService lessonMatchingService;
 
+    // Student가 Tutor에게 레슨 요청을 보냄
     @PostMapping("/{tutorId}/matching")
     public LessonMatchingResponse lessonMatching(
             @AuthenticationPrincipal BoardPrincipal boardPrincipal,
@@ -22,6 +27,15 @@ public class LessonMatchingController {
             @RequestBody LessonMatchingRequest request
     ) {
         return lessonMatchingService.lessonMatching(boardPrincipal, tutorId, request);
+    }
+
+    // Tutor는 자신의 matching 정보들을 list로 확인
+    @PreAuthorize("hasRole('TUTOR')")                       // "ROLE_TUTOR" 권한이 있으면 통과
+    @GetMapping("/my/matchings")
+    public List<MyMatchingResponse> myMatchings(
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal
+    ) {
+        return lessonMatchingService.myMatchings(boardPrincipal);
     }
 
 }

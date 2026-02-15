c2b94dda74a9c59bb4d5c8691c098ba260a63cfc
yoojanghun <yoojanghun@naver.com>
2026-02-15 19:13:05 +0900

    #33 - Review 작성 Service layer 설계
    LessonReview 객체 만들 때, matching 객체 필요
    이 때, matching 객체는 studentId와 MatchingStatus.ACCEPTED로 필터링

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/repository/MatchingRepository.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/repository/MatchingRepository.java
@@ -2,9 +2,13 @@ package com.lessonmatchingplatform.lesson_matching_platform.repository;
 
 import com.lessonmatchingplatform.lesson_matching_platform.domain.lesson.Matching;
 import com.lessonmatchingplatform.lesson_matching_platform.repository.querydsl.MatchingRepositoryCustom;
+import com.lessonmatchingplatform.lesson_matching_platform.type.MatchingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MatchingRepository extends JpaRepository<Matching, Long>, MatchingRepositoryCustom {
+    Optional<Matching> findByStudentAccount_StudentIdAndStatus(Long studentId, MatchingStatus status);
 }

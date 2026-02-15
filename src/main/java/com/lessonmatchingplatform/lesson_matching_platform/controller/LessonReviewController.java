44ed11f65cae00cde7e08ede88070bf1b06c3ebe
yoojanghun <yoojanghun@naver.com>
2026-02-15 19:06:27 +0900

    #33 - 레슨 리뷰 작성 Controller 만듦

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/controller/LessonReviewController.java
@@ -0,0 +1,36 @@
+package com.lessonmatchingplatform.lesson_matching_platform.controller;
+
+import com.lessonmatchingplatform.lesson_matching_platform.dto.request.ReviewRequest;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.response.ReviewResponse;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.response.TutorWithReviewsResponse;
+import com.lessonmatchingplatform.lesson_matching_platform.dto.security.BoardPrincipal;
+import com.lessonmatchingplatform.lesson_matching_platform.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/tutors")
+@RestController
+public class LessonReviewController {
+
+    private final ReviewService reviewService;
+
+    // 한 선생님의 레슨 페이지에 리뷰 추가
+    @PreAuthorize("hasRole('STUDENT')")
+    @PostMapping("/{tutorId}/reviews")
+    public ReviewResponse postReview(
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+            @PathVariable Long tutorId,
+            @RequestBody ReviewRequest request
+    ) {
+        return reviewService.postReview(boardPrincipal, request, tutorId);
+    }
+
+    // 선생님의 레슨정보와 학생들의 리뷰를 한꺼번에 보여주기
+//    @GetMapping("/{tutorId}/reviews")
+//    public TutorWithReviewsResponse getTutorAndReviews(
+//
+//    )
+}

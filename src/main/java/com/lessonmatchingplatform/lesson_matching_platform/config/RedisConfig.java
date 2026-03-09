313ac0097900e3edc51b3879e376d464e5ad42e1
yoojanghun <yoojanghun@naver.com>
2026-03-09 18:17:49 +0900

    redis cache 설정

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/config/RedisConfig.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/config/RedisConfig.java
@@ -1,15 +1,23 @@
 package com.lessonmatchingplatform.lesson_matching_platform.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -35,5 +43,17 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(RedisSerializer.json());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()))
+                .entryTtl(Duration.ofHours(1));                 // 캐시 유효 시간 (1시간)
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }

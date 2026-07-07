68b6481735b6b5b9eece970d59cb0ad0ab86f62c
yoojanghun <yoojanghun@naver.com>
2026-07-07 23:30:18 +0900

    LessonMatchingPlatformApplication.java에서 @ConfigurationPropertiesScan 추가
    @ConfigurationProperties가 붙은 클래스들을 전부 자동으로 찾아내어 스프링 빈으로 등록해주는 역할

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/LessonMatchingPlatformApplication.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/LessonMatchingPlatformApplication.java
@@ -2,7 +2,9 @@ package com.lessonmatchingplatform.lesson_matching_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class LessonMatchingPlatformApplication {
 

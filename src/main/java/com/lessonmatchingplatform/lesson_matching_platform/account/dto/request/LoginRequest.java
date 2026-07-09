d2a23c4a7ab784677548ab0cc490c917d0a38191
yoojanghun <yoojanghun@naver.com>
2026-07-10 01:13:32 +0900

    LoginRequest 생성하는 정적 팩토리 메서드 추가

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/account/dto/request/LoginRequest.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/account/dto/request/LoginRequest.java
@@ -3,4 +3,8 @@ package com.lessonmatchingplatform.lesson_matching_platform.account.dto.request;
 public record LoginRequest(
         String username,
         String password
-) {}
+) {
+    public static LoginRequest of(String username, String password) {
+        return new LoginRequest(username, password);
+    }
+}

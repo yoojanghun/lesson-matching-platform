f2ad11fe9c909125d0d6c3cb648ba838a82df2bf
yoojanghun <yoojanghun@naver.com>
2026-07-18 23:25:14 +0900

    #46 - 학생이 특정 시간에 한 레슨 예약을 저장하는 Reservation Entity 추가
    ReservationStatus를 ENUM 타입으로 두어 4가지 상태 중 하나만 선택할 수 있도록 함.

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/lesson/domain/Reservation.java
@@ -0,0 +1,74 @@
+package com.lessonmatchingplatform.lesson_matching_platform.lesson.domain;
+
+import com.lessonmatchingplatform.lesson_matching_platform.account.domain.TutorAccount;
+import com.lessonmatchingplatform.lesson_matching_platform.global.domain.AuditingFields;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+@ToString(callSuper = true)
+@Getter
+@Entity
+public class Reservation extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reservationId;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "matching_id", nullable = false)
+    private Matching matching;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tutor_id", nullable = false)
+    private TutorAccount tutorAccount;
+
+    @Column(length = 500)
+    private String requestMsg;
+
+    @Column(nullable = false)
+    private LocalDate lessonDate;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    @Column(nullable = false)
+    private ReservationStatus reservationStatus;
+
+    protected Reservation() {}
+
+    private Reservation(Matching matching, TutorAccount tutorAccount, String requestMsg, LocalDate lessonDate, LocalTime startTime, LocalTime endTime, ReservationStatus reservationStatus) {
+        this.matching = matching;
+        this.tutorAccount = tutorAccount;
+        this.requestMsg = requestMsg;
+        this.lessonDate = lessonDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.reservationStatus = reservationStatus;
+    }
+
+    public static Reservation of(Matching matching, TutorAccount tutorAccount, String requestMsg, LocalDate lessonDate, LocalTime startTime, LocalTime endTime, ReservationStatus reservationStatus) {
+        return new Reservation(matching, tutorAccount, requestMsg, lessonDate, startTime, endTime, reservationStatus);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reservation that)) return false;
+        return this.reservationId != null && Objects.equals(this.reservationId, that.reservationId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(reservationId);
+    }
+}

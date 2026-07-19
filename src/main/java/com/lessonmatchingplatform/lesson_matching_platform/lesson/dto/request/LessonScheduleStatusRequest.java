8606dcb4b00f745c73a7430d00837994d7b400da
yoojanghun <yoojanghun@naver.com>
2026-07-20 00:45:48 +0900

    #46 - LessonScheduleStatusRequest request DTO 작성
    학생이 레슨을 예약하여 PENDING 상태일 때, 레슨을 승인하거나 거절, 취소하는 정보를 받기 위한 DTO

---
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/lesson/dto/request/LessonScheduleStatusRequest.java
@@ -0,0 +1,22 @@
+package com.lessonmatchingplatform.lesson_matching_platform.lesson.dto.request;
+
+import com.lessonmatchingplatform.lesson_matching_platform.lesson.domain.ReservationStatus;
+import jakarta.validation.constraints.NotNull;
+
+public record LessonScheduleStatusRequest(
+
+        @NotNull(message = "레슨 상태 값은 필수입니다.")
+        ReservationStatus reservationStatus
+) {
+
+    public LessonScheduleStatusRequest {
+        if (reservationStatus != null) {
+            if (reservationStatus != ReservationStatus.CONFIRMED &&
+                    reservationStatus != ReservationStatus.REJECTED &&
+                    reservationStatus != ReservationStatus.CANCELLED
+            ) {
+                throw new IllegalStateException("튜터가 변경할 수 없는 예약 상태 값입니다.");
+            }
+        }
+    }
+}

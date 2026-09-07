9eb6747d2dc7a9d1f6ad7ea877b5d2370bbdf897
yoojanghun <yoojanghun@naver.com>
2026-09-07 21:30:00 +0900

    #70 - TutorAccount 회원 탈퇴 등의 작업을 처리하는 로직 작성

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/account/service/ProfileService.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/account/service/ProfileService.java
@@ -19,6 +19,7 @@ import com.lessonmatchingplatform.lesson_matching_platform.category.repository.C
 import com.lessonmatchingplatform.lesson_matching_platform.category.repository.SubjectRepository;
 import com.lessonmatchingplatform.lesson_matching_platform.account.dto.response.TutorProfileResponse;
 import com.lessonmatchingplatform.lesson_matching_platform.tutor.repository.TutorsRepository;
+import com.lessonmatchingplatform.lesson_matching_platform.tutor.search.event.TutorSyncEventPublisher;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
@@ -408,6 +409,16 @@ public class ProfileService {
         }
 
         tutorAccount.updateProfileCompletionStatus();
+        tutorSyncEventPublisher.publishSaveEvent(tutorId);
+    }
+
+    @CacheEvict(value = "tutorDetail", key = "#tutorId")
+    public void deleteMyTutorProfile(Long tutorId) {
+        TutorAccount tutorAccount = tutorsRepository.findProfileById(tutorId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 강사를 찾을 수 없습니다. id=" + tutorId));
+
+        tutorAccount.resetProfile();
+        tutorSyncEventPublisher.publishDeleteEvent(tutorId);
     }
 
 }

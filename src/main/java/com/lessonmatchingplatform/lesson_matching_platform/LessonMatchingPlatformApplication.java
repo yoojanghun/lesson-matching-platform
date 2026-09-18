3ec99cded4fc9537e8bf93b3aba6e22c7be90df4
yoojanghun <yoojanghun@naver.com>
2026-09-19 00:07:47 +0900

    #73 - PageImpl 객체를 JSON으로 직렬화할 때 발생하는 호환성 에러를 방지
    기존 방식의 문제: PageImpl을 그대로 반환하면 Spring Data 내부 구조가 JSON에 노출되어,
    프레임워크 버전이 올라갈 때 JSON 구조가 변경되어 프론트엔드 코드 데이터 포맷이 깨지는 문제가 발생
    
    VIA_DTO 설정의 역할: Spring Data가 제공하는 표준 DTO 형식(PagedModel)으로
    Page 객체를 자동으로 포장하여 안전하게 직렬화

--- src/main/java/com/lessonmatchingplatform/lesson_matching_platform/LessonMatchingPlatformApplication.java
+++ src/main/java/com/lessonmatchingplatform/lesson_matching_platform/LessonMatchingPlatformApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableCaching
 @ConfigurationPropertiesScan
 @SpringBootApplication
